730560ea3aa9934bb7a7f1c8cde2fc4416d37efe
JonesQuito <jonesdhy@hotmail.com>
2018-04-14 12:02:10 -0300

    Remoção do spring-security

--- src/main/java/com/algaworks/festa/config/InMemorySecurityConfig.java
+++ src/main/java/com/algaworks/festa/config/InMemorySecurityConfig.java
@@ -1,12 +1,8 @@
 package com.algaworks.festa.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-
-@Configuration
+//@Configuration
 public class InMemorySecurityConfig {
-	
+	/*
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
 		builder
@@ -16,5 +12,5 @@ public class InMemorySecurityConfig {
 			.withUser("alexandre").password("123").roles("USER")
 			.and()
 			.withUser("thiago").password("123").roles("USER");
-	}
+	}*/
 }
